fd064545ed98f7d43fd7f64a385c27b359127fc9
Shalom-302 <54105227+Shalom-302@users.noreply.github.com>
2025-04-12 20:08:08 +0000

    first version of chatbot

---
+++ main.py
@@ -0,0 +1,21 @@
+import streamlit as st
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.schema import HumanMessage
+from dotenv import load_dotenv
+import os
+import google.generativeai as genai
+load_dotenv()
+
+apikey = os.getenv("GOOGLE_API_KEY")
+genai.configure(api_key=apikey)
+
+
+llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro")
+
+st.title("💬 Chat avec Gemini (LangChain)")
+
+question = st.text_input("Pose ta question :")
+
+if question:
+    response = llm([HumanMessage(content=question)])
+    st.write("**Gemini :**", response.content)
